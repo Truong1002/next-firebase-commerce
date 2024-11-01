f8ab35cd8b5eccd9cf33eb90c98d9e9e291e16ce
mega <truongphamxuan724@gmail.com>
2024-11-01 09:40:08 +0700

    [Bài 18: ] Xây dựng giao diện layout trang Admin

---
+++ src/app/admin/(private)/managers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const managers = () => {
+  return (
+    <div>managers</div>
+  )
+}
+
+export default managers
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -4,3 +4,4 @@ export const COLLECTIONS = {
     PRODUCT: "products",
     CART: "carts",
   };
+
